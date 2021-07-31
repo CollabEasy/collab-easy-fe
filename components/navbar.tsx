349f1a9a45a4d0d8caf1426ccfea4f738e5da17b
rahul gupta <rabbal1892@rabbal1892.local>
2021-07-31 16:39:41 -0500

    some changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,48 +1,52 @@
 import Link from "next/link";
+import Title from "./title";
 import { useRoutesContext } from "./routeContext";
 import styles from '../public/styles/navbar.module.scss';
+import React from 'react'
+import { Nav, Button } from 'react-bootstrap';
+
+// I took the code from here https://www.gyanblog.com/javascript/next-js-bootstrap-template-navbar-header/
 
 const Navbar = () => {
   const { toLogin, toSignup, toDiscover, toWondorHome } = useRoutesContext()
   return (
-    <nav className="navbar navbar-light bg-light hdr-clr">
-      <div className="container-fluid">
-        <Link href={toWondorHome().href}>
+    <Nav className="navbar navbar-expand-lg navbar-light hdr-clr " style={{padding: '20px'}}>
+      <div className="container-xl">
+      <Link href={toWondorHome().href}>
           <a className={'navbar-brand ' + styles.appLogo}>Wondor</a>
         </Link>
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <Link href={toDiscover().href}>
-              <a className="nav-link active" aria-current="page">
-                Discover
-              </a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href={toDiscover().href}>
-              <a className="nav-link" aria-current="page">
-                About
-              </a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href={toLogin().href}>
-              <a className="nav-link">
-                Login
-              </a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href={toSignup().href}>
-              <a className="nav-link">
-                Sign up
-              </a>
-            </Link>
-          </li>
-        </ul>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="justify-content-end collapse navbar-collapse" id="navbarsExample07XL">
+          <ul className="nav">
+            <li className="nav-item active">
+              <Link href={toDiscover().href}>
+                <a className="nav-link">Discover</a>
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href={toDiscover().href}>
+              <a className="nav-link" aria-current="page">About</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href={toLogin().href}>
+                <a className="nav-link">Login</a>
+              </Link>
+            </li>
+            <li className="nav-item text-nowrap">
+              <Link href={toSignup().href}>
+                <a className="nav-link">Sign up</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </nav>
-  );
+    </Nav>
+  )
 }
 
 export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import Title from '../components/title'
 import { useSelector } from 'react-redux'
 import { AppState } from '../types/core';
 import Image from 'next/image';
-import profilePic from '../public/images/landing-1.png';
+import landingPageImg from '../public/images/landing-1.png';
+import styles from '../public/styles/landing-page.module.scss';
+
+
 const Home = () => {
   const myState = useSelector((state: AppState) => state.home);
 
@@ -10,27 +13,57 @@ const Home = () => {
   return (
     <>
       <Title title="Collab Easy ..." />
-      <div className='container-fluid m-0'>
-        <div className="row">
-          <div className="col-md-12 m-0 p-0">
-            <Image src={profilePic} alt="Picture of the author" />
-          </div>
-          <div className="col-md-12 col-sm-12 src-prnt">
-            <div className="form-floating mb-3 col-md-4 col-sm-6 src-box">
-              <input type="text" className="form-control" id="floatingInput" placeholder="" />
-              <label className="label-search" htmlFor="floatingInput">Search Category, users, etc.</label>
-              <span className="fa-cion"><em className="fa fa-search" aria-hidden="true"></em></span>
+      <div>
+        <div className="container-fluid">
+          <div className="container box">
+            <div className='container-fluid m-0'>
+              <div className="row">
+                <div className="col-md-12 m-0 p-0">
+                  <Image className={'container-fluid' + styles.searchbox} src={landingPageImg} alt="Landing page" />
+                </div>
+                <div className="col-md-12 col-sm-12 src-prnt">
+                  <div className="form-floating mb-3 col-md-4 col-sm-6 src-box">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="" />
+                    <label className="label-search" htmlFor="floatingInput">Search Category, users, etc.</label>
+                    <span className="fa-cion"><em className="fa fa-search" aria-hidden="true"></em></span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row p-t-10 row-sec">
-          <div className="col-md-12 m-0 p-0">
-            <Image src={profilePic} alt="Picture of the author" />
+
+        <div className="container-fluid" style={{ padding: '20px' }}>
+          <div className="row welcome text-left">
+            <div className="col-12">
+              <h2>Popular Categories.</h2>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="container fluid">
+          <div className="row text-center padding">
+            <div className='col-xs-12 col-sm-6 col-md-3 speciality'>
+              <Image src={landingPageImg} alt="Listing of online prescription treatment options for erectile dysfunction and premature ejaculation." />
+              <h6>Women's Health</h6>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3 speciality">
+              <Image src={landingPageImg} alt="Listing of online prescription treatment options for erectile dysfunction and premature ejaculation." />
+              <h6>Skin Care</h6>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3 speciality">
+              <Image src={landingPageImg} alt="Listing of online prescription treatment options for erectile dysfunction and premature ejaculation." />
+              <h6>Men's Health</h6>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3 speciality">
+              <Image src={landingPageImg} alt="Listing of online prescription treatment options for erectile dysfunction and premature ejaculation." />
+              <h6>Hair Fall</h6>
+            </div>
           </div>
         </div>
       </div>
     </>
-
   )
 }
 
