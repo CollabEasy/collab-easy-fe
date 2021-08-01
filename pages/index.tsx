b7e3174e166d64ee0c0536646565ba0107204e5d
rahul gupta <rabbal1892@rabbal1892.local>
2021-08-01 18:31:47 -0500

    more style

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ import singersImg from '../public/images/singers.png';
 import paintersImg from '../public/images/painter.png';
 import dancersImg from '../public/images/dancers.png';
 import inspireImg from '../public/images/inspire.png';
+import galleryImg1 from '../public/images/gallery1.jpg';
+import galleryImg2 from '../public/images/gallery2.jpg';
+
 import styles from '../public/styles/index.module.scss';
 import { Card } from 'antd';
 
@@ -22,68 +25,108 @@ const Home = () => {
       <Title title="Collab Easy ..." />
       <div>
         <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <Image className={'container-fluid' + styles.searchbox} src={landingPageImg} alt="Landing page" />
+          <div className="row">
+            <div className="col-md-12">
+              <Image className={styles.searchbox} src={landingPageImg} alt="Landing page" />
+            </div>
+            <div className="col-md-12 col-sm-12 src-prnt">
+              <div className="form-floating mb-3 col-md-4 col-sm-6 src-box">
+                <input type="text" className="form-control" id="floatingInput" placeholder="" />
+                <label className="label-search" htmlFor="floatingInput">Search Category, users, etc.</label>
+                <span className="fa-cion"><em className="fa fa-search" aria-hidden="true"></em></span>
               </div>
-              <div className="col-md-12 col-sm-12 src-prnt">
-                <div className="form-floating mb-3 col-md-4 col-sm-6 src-box">
-                  <input type="text" className="form-control" id="floatingInput" placeholder="" />
-                  <label className="label-search" htmlFor="floatingInput">Search Category, users, etc.</label>
-                  <span className="fa-cion"><em className="fa fa-search" aria-hidden="true"></em></span>
-                </div>
-              </div>
+            </div>
           </div>
         </div>
 
         <div className={'container fluid'}>
           <div className="row col-12 text-left">
-              <h2 className={styles['padding']}>Popular categories</h2>
+            <h2 className={styles['padding']}>Popular categories</h2>
           </div>
         </div>
 
         <div className="container fluid">
-          <div className={"row flex-row flex-nowrap mt-4 pb-4 pt- " + styles['scrolling-wrapper']}>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2"}>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={musiciansImg} alt="cards" />}></Card>
+          <div className={"row flex-row flex-nowrap mt-4 pb-4 pt-2" + styles['scrolling-wrapper']}>
+            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
+              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={musiciansImg} alt="cards" />}>
+                {/* <Meta title="Musicians" /> */}
+              </Card>
             </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2" }>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={singersImg} alt="cards" />}></Card>
+            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
+              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={singersImg} alt="cards" />}>
+                {/* <Meta title="Musicians" /> */}
+              </Card>
             </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2" }>
+            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
               <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={paintersImg} alt="cards" />}></Card>
             </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2" }>
+            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
               <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
             </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2" }>
+            {/* <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3" }>
               <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
-            </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2" }>
+            </div> */}
+            {/* <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3" }>
               <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
-            </div>
+            </div> */}
           </div>
         </div>
 
         <div className={'container fluid'}>
           <div className="row col-12 text-left">
-              <h2 className={styles['padding']}>Featured art</h2>
-            </div>
+            <h2 className={styles['padding']}>Featured artwork</h2>
+          </div>
         </div>
 
         {/* look at gallery options here: https://freefrontend.com/bootstrap-galleries/ */}
 
         <div className="container fluid">
-          {/* "add gallery code" */}
-        </div >
-
-       <div className="container fluid">
           <div className="row">
-            <div className="col-md-12 m-0 p-0">
-              <Image className={'container-fluid' + styles.searchbox} src={inspireImg} alt="Landing page" />
+            <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+              <Image
+                src={galleryImg1}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
+              <Image
+                 src={galleryImg2}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <Image
+                 src={galleryImg2}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
+              <Image
+                src={galleryImg1}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <Image
+                 src={galleryImg2}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
+              <Image
+                src={galleryImg1}
+                className="w-100 shadow-1-strong rounded mb-4"
+                alt=""
+              />
             </div>
           </div>
-        </div> 
+        </div >
+        <div className="container fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <Image className={styles.searchbox + styles['padding']} src={inspireImg} alt="Landing page" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
