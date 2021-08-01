d4d336957dd16422493e314bb008ff68ac0845f7
rahul gupta <rabbal1892@rabbal1892.local>
2021-07-31 21:44:55 -0500

    fixed navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,51 +2,35 @@ import Link from "next/link";
 import Title from "./title";
 import { useRoutesContext } from "./routeContext";
 import styles from '../public/styles/navbar.module.scss';
-import React from 'react'
-import { Nav, Button } from 'react-bootstrap';
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Button, Navbar, NavDropdown } from 'react-bootstrap';
 
-// I took the code from here https://www.gyanblog.com/javascript/next-js-bootstrap-template-navbar-header/
 
-const Navbar = () => {
+// I took the code from here https://stackoverflow.com/questions/62609559/navbar-collapse-button-does-not-show-items-for-bootstrap-4-5-0-and-nextjs-9-4-4
+
+const Navbarr = () => {
   const { toLogin, toSignup, toDiscover, toWondorHome } = useRoutesContext()
   return (
-    <Nav className="navbar navbar-expand-lg navbar-light hdr-clr " style={{padding: '20px'}}>
-      <div className="container-xl">
-      <Link href={toWondorHome().href}>
-          <a className={'navbar-brand ' + styles.appLogo}>Wondor</a>
-        </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <Navbar expand="lg" id="myNavbar" className="container-xl">
 
-        <div className="justify-content-end collapse navbar-collapse" id="navbarsExample07XL">
-          <ul className="nav">
-            <li className="nav-item active">
-              <Link href={toDiscover().href}>
-                <a className="nav-link">Discover</a>
-              </Link>
-            </li>
+      <Navbar.Brand href="#home">
+        <Link href={toWondorHome().href}>
+           <a className={'navbar-brand ' + styles.appLogo}>Wondor</a>
+         </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav className="ml-auto" id="myNavItem">
+          <Nav.Link href={toDiscover().href} id="myNavItem">Discover</Nav.Link>
+          <Nav.Link href={toDiscover().href} id="myNavItem">About</Nav.Link>
+          <Nav.Link href={toLogin().href} id= "myNavItem">Log in</Nav.Link>
+          <Nav.Link href={toSignup().href} id= "myNavItem">Sign up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
 
-            <li className="nav-item">
-              <Link href={toDiscover().href}>
-              <a className="nav-link" aria-current="page">About</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href={toLogin().href}>
-                <a className="nav-link">Login</a>
-              </Link>
-            </li>
-            <li className="nav-item text-nowrap">
-              <Link href={toSignup().href}>
-                <a className="nav-link">Sign up</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </Nav>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default Navbarr
