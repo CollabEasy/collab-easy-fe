e0d856bf690edce3e5b6e8b584044082566f288f
rahul gupta <rabbal1892@rabbal1892.local>
2021-07-31 21:48:23 -0500

    fixed a few unused imports

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,16 @@
 import Link from "next/link";
-import Title from "./title";
 import { useRoutesContext } from "./routeContext";
 import styles from '../public/styles/navbar.module.scss';
-import React, {Component} from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav, Button, Navbar, NavDropdown } from 'react-bootstrap';
+import {Nav, Navbar } from 'react-bootstrap';
 
 
 // I took the code from here https://stackoverflow.com/questions/62609559/navbar-collapse-button-does-not-show-items-for-bootstrap-4-5-0-and-nextjs-9-4-4
-
 const Navbarr = () => {
   const { toLogin, toSignup, toDiscover, toWondorHome } = useRoutesContext()
   return (
     <Navbar expand="lg" id="myNavbar" className="container-xl">
-
       <Navbar.Brand href="#home">
         <Link href={toWondorHome().href}>
            <a className={'navbar-brand ' + styles.appLogo}>Wondor</a>
@@ -23,12 +20,11 @@ const Navbarr = () => {
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="ml-auto" id="myNavItem">
           <Nav.Link href={toDiscover().href} id="myNavItem">Discover</Nav.Link>
-          <Nav.Link href={toDiscover().href} id="myNavItem">About</Nav.Link>
+          <Nav.Link href={toDiscover().href} id="myNavItem">About us</Nav.Link>
           <Nav.Link href={toLogin().href} id= "myNavItem">Log in</Nav.Link>
           <Nav.Link href={toSignup().href} id= "myNavItem">Sign up</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-
     </Navbar>
   )
 }
