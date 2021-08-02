34ee3b4fc05fe2e5bd50d697dcc7cd1e05f45c86
rahul gupta <rabbal1892@rabbal1892.local>
2021-08-01 23:13:15 -0500

    added gallery

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,80 +46,24 @@ const Home = () => {
         </div>
 
         <div className="container fluid">
-          <div className={"row flex-row flex-nowrap mt-4 pb-4 pt-2" + styles['scrolling-wrapper']}>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={musiciansImg} alt="cards" />}>
-                {/* <Meta title="Musicians" /> */}
-              </Card>
-            </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={singersImg} alt="cards" />}>
-                {/* <Meta title="Musicians" /> */}
-              </Card>
-            </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={paintersImg} alt="cards" />}></Card>
-            </div>
-            <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3"}>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
-            </div>
-            {/* <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3" }>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
-            </div> */}
-            {/* <div className={"col-6 col-sm-4 col-md-4 col-lg-3 col-xl-3" }>
-              <Card hoverable bordered={false} style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}></Card>
-            </div> */}
-          </div>
-        </div>
-
-        <div className={'container fluid'}>
-          <div className="row col-12 text-left">
-            <h2 className={styles['padding']}>Featured artwork</h2>
-          </div>
-        </div>
-
-        {/* look at gallery options here: https://freefrontend.com/bootstrap-galleries/ */}
-
-        <div className="container fluid">
-          <div className="row">
-            <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-              <Image
-                src={galleryImg1}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
-              <Image
-                 src={galleryImg2}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
-            </div>
-            <div className="col-lg-4 mb-4 mb-lg-0">
-              <Image
-                 src={galleryImg2}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
-              <Image
-                src={galleryImg1}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
-            </div>
-            <div className="col-lg-4 mb-4 mb-lg-0">
-              <Image
-                 src={galleryImg2}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
-              <Image
-                src={galleryImg1}
-                className="w-100 shadow-1-strong rounded mb-4"
-                alt=""
-              />
+          <div className="col-lg-12 col-sm-6 col-md-3">
+            <div className={styles["cover-container"]}>
+              <div className={styles['cover-item']}>
+                <Image src={musiciansImg} />
+              </div>
+              <div className={styles['cover-item']}>
+                <Image src={singersImg} />
+              </div>
+              <div className={styles['cover-item']}>
+                <Image src={paintersImg} />
+              </div>
+              <div className={styles['cover-item']}>
+                <Image src={dancersImg} />
+              </div>
             </div>
           </div>
-        </div >
+      </div>
+
         <div className="container fluid">
           <div className="row">
             <div className="col-md-12">
@@ -128,6 +72,59 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className={'container fluid'}>
+        <div className="row col-12 text-left">
+          <h2 className={styles['padding']}>Featured artwork</h2>
+        </div>
+      </div>
+
+      <div className="container fluid">
+        <div className={styles["grid-container"]}>
+          <div>
+            <img className="grid-item grid-item-1" src='https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"I'm so happy today!"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-2' src='https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"I see those nugs."</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-3' src='https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"I love you so much!"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-4' src='https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"I'm the baby of the house!"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-5' src='https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='/' />
+            <p>"Are you gunna throw the ball?"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-6' src='https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"C'mon friend!"</p>
+          </div>
+
+          <div>
+            <img className='grid-item grid-item-7' src='https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"A rose for mommy!"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-8' src='https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"You gunna finish that?"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-9' src='https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"We can't afford a cat!"</p>
+          </div>
+          <div>
+            <img className='grid-item grid-item-10' src='https://images.unsplash.com/photo-1504595403659-9088ce801e29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='' />
+            <p>"Dis my fren!"</p>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
