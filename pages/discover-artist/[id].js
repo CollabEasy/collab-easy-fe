e7f644a239fe3cc988ef5f4b7136b4edc45deccb
rahul gupta <rabbal1892@rabbal1892.local>
2021-08-03 22:32:46 -0500

    added code for listing page

--- pages/discover-artist/[id].js
+++ pages/discover-artist/[id].js
@@ -1,6 +1,11 @@
-
+import Image from 'next/image';
 import Title from './../../components/title';
 import { useRouter } from 'next/router'
+import landingPageImg from '../../public/images/listing.png';
+import dancersImg from '../../public/images/dancers.png';
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 const DiscoverArtist = () => {
 
@@ -8,11 +13,29 @@ const DiscoverArtist = () => {
   const { id } = router.query;
   console.log(router, id, '<=== ')
 
+  const array = Array(7).fill(undefined)
+
   return (
-    <div className='container'>
+    <>
       <Title title="Discover Artist" />
-      <h1>Artist Page Dynamic <strong>{id.toUpperCase()}</strong></h1>
-    </div>
+      <div className="container fluid">
+        <div className="row" style={{paddingTop: '3rem'}}>
+              <Image src={landingPageImg} alt="Landing page" />
+        </div>
+        {/* <h1>Artist Page Dynamic <strong>{id.toUpperCase()}</strong></h1> */}
+
+          <div className="row" style={{paddingTop:'2rem'}}>
+          {array.map(_ => (
+            <div className="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3" style={{padding: '1rem'}}>
+              <Card hoverable style={{ height: '100%' }} cover={<Image src={dancersImg} alt="cards" />}>
+                <Meta title="Dancers" />
+              </Card>
+            </div>
+          ))}
+            
+        </div> 
+        </div>
+    </>
   )
 }
 
