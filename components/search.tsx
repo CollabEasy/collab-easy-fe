daa9ff8ef50310447cdeaae3e6e2bf523a473999
Abhishek Arora <abbhishek.arora1993@gmail.com>
2021-08-21 23:00:12 +0530

    added onChange handler and cross button for search

--- components/search.tsx
+++ components/search.tsx
@@ -1,21 +1,41 @@
-import { SearchOutlined } from '@ant-design/icons';
-import styles from '../public/styles/searchBar.module.scss';
+import React, { useState } from 'react';
+import { SearchOutlined, CloseOutlined } from '@ant-design/icons';
+import styles from '../styles/searchBar.module.scss';
 const Search = () => {
+    const [inputVal, setInputVal] = useState('');
+    
+    const handleChange = (event) => {
+        setInputVal(event.target.value);
+    }
+
+    const clearInput = () => {
+        setInputVal('');
+    }
 
     return (
         <>
             <div className={styles["search-bar"]}>
+                <button className={styles["search-bar__submit"]}>
+                    <SearchOutlined style={{ fontSize: '20px'}} />
+                </button>
                 <input 
                     type="text" 
                     className={styles["search-bar__input"]} 
                     placeholder="search artists"
                     aria-label="search"
+                    value={inputVal}
+                    onChange={handleChange}
                 >
-
                 </input>
-                <button className={styles["search-bar__submit"]}>
-                    <SearchOutlined style={{ fontSize: '20px'}} />
-                </button>
+                {
+                    inputVal.length > 0 && 
+                    <button 
+                        className={styles["search-bar__cross"]}
+                        onClick={clearInput}
+                    >
+                        <CloseOutlined style={{ fontSize: '16px', paddingBottom: '5px'}} />
+                    </button> 
+                }
             </div>
         </>
     );
