0cb23404cb43e011395b1a739b2dcad5efeb9754
Abhishek Arora <abbhishek.arora1993@gmail.com>
2021-08-30 21:08:13 +0530

    fixed review comments

--- components/search.tsx
+++ components/search.tsx
@@ -7,17 +7,43 @@ import {
 import { Subject, merge, of } from 'rxjs';
 const Search = () => {
     const [inputVal, setInputVal] = useState('');
-    const [searchState, setSearchState] = useState({
-        data: [],
-        loading: false,
-        errorMessage: '',
-        noResults: false
-    });
+    const [searchData, setSearchData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [noResults, setNoResults] = useState(false);
+
+    const setSearchState = (
+        data: Array<object>, loading: boolean, errorMessage: string, noResults: boolean
+    ) => {
+        setSearchData(data);
+        setLoading(loading);
+        setErrorMessage(errorMessage);
+        setNoResults(noResults);
+    };
+
     const [onSearch$] = useState(() => new Subject());
     
     const handleTextChange = async (event) => {
         setInputVal(event.target.value);
         onSearch$.next(event.target.value);
+    }
+
+    const callSearchAPI = async (val:string) => {
+        try {
+            /* Type 'any' is of type Array<object> but getting some error */
+            const res: any = await getSearchResult(val);
+            return {
+                data: res,
+                loading: false,
+                noResults: res.length === 0,
+            };
+        } catch(err) {
+            return {
+                data: [],
+                loading: false,
+                errorMessage: err.message
+            }
+        }
     }
 
     useEffect(() => {
@@ -26,31 +52,19 @@ const Search = () => {
             distinctUntilChanged(),
             filter(val => val.length >= 2),
             debounceTime(400),
-            tap(val => console.log(`Calling APi with ${val}`)),
-            switchMap((val:string) => merge(
-                of({loading: true, errorMessage: '', noResults: false}),
-                getSearchResult(val).then((res: Array<object>) => {
-                    console.log(res);
-                    return {
-                        data: res,
-                        loading: false,
-                        noResults: res.length === 0,
-                    };
-
-                }).catch((err) => {
-                    return {
-                        data: [],
-                        loading: false,
-                        errorMessage: err.message
-                    }
-                })
-            )),
+            switchMap((val:string) => 
+                merge(
+                    of({data: [], loading: true, errorMessage: '', noResults: false}),
+                    callSearchAPI(val),
+                )
+            ),
             /* catchError(e => ({
                 loading: false,
                 errorMessage: 'An application error occured'
             })) */
         ).subscribe(newSearchState => {
-            setSearchState(s => Object.assign({}, s, newSearchState));
+            const { data, loading, errorMessage, noResults } = newSearchState;
+            setSearchState(data, loading, errorMessage, noResults);
         });
 
         return () => {
