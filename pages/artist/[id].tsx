96024c71ebc35f28dd58a53f9afd33eaa802734d
rahul gupta <rabbal1892@rabbal1892.local>
2021-09-12 20:58:20 -0500

    added basic UI for bathla

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -2,6 +2,56 @@ import Title from '../../components/title';
 import Image from 'next/image';
 import avatar from '../../public/images/avatar.png'
 import React, { useEffect } from 'react';
+import { Table, Pagination, Space } from 'antd';
+import { Button, Card, Avatar } from 'antd';
+
+// https://ant.design/components/card/
+const { Meta } = Card;
+
+// https://ant.design/components/table/
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    theme: "independence day",
+    description: 'collage',
+  },
+  {
+    key: '2',
+    name: 'John',
+    theme: "Black Friday",
+    description: 'lets make a movie',
+  },
+];
+
+const columns = [
+  {
+    title: 'Sender Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Theme',
+    dataIndex: 'theme',
+    key: 'theme',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => (
+      <Space size="middle">
+        <a>Accept </a>
+        <a>Reject</a>
+      </Space>
+    ),
+  },
+];
+
 
 /**
  * @description On Click tab active the window
@@ -70,6 +120,7 @@ const ArtistProfile = () => {
               <div className="tab">
                 <button id="about_1" className="tablinks active about" onClick={() => toggleTab('about')}>About</button>
                 <button id="sample_1" className="tablinks sample" onClick={() => toggleTab('sample')}>My sample work</button>
+                <button id="collab_1" className="tablinks collab" onClick={() => toggleTab('collab')}>Collab Requests</button>
               </div>
             </div>
             <div className="col-xl-4 col-md-4 col-sm-4">
@@ -83,6 +134,34 @@ const ArtistProfile = () => {
               <div id="sample" className="tabcontent">
                 <p className="f-w-b">Sample</p>
                 <p>Sample 1</p>
+              </div>
+              <div id="collab" className="tabcontent">
+                <h4 className="f-w-b">Pending Request</h4>
+                <Table dataSource={dataSource} columns={columns} />
+                <h4 className="f-w-b">Scheduled Request</h4>
+                <Table dataSource={dataSource} columns={columns} />
+                <h4 className="f-w-b">Completed Request</h4>
+                <Table dataSource={dataSource} columns={columns} />
+                <h4 className="f-w-b">Rejected Request</h4>
+                
+                <Card
+                // we can put this card in loop here.
+                  style={{ width: 300 }}
+                  cover={
+                    <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"/>
+                  }
+                  actions={[
+                    <Button type="primary">Accept</Button>,
+                    <Button type="primary">Reject</Button>,
+                  ]}
+                >
+                  <Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title="Independence Day"
+                    description="Let's make a vide on it."
+                  />
+                </Card>
+                <Pagination defaultCurrent={1} total={50}/>
               </div>
             </div>
           </div>
