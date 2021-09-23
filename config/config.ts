0d8df2c7fdebc597dc5ae29dc9b4339654662cb7
Ankit Bathla <ankitbathla@chegg.com>
2021-09-23 20:44:50 +0530

    build fix 1

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,6 @@ import { AppEnvs, NodeEnvs } from "./environment";
 export const Config = {
   appEnv: process.env.APP_ENV || AppEnvs.DEVELOPMENT,
   nodeEnv: process.env.NODE_ENV || NodeEnvs.DEVELOPMENT,
-  backendEndpoint: process.env.API_ENDPOINT || 'http://localhost:3001',
+  backendEndpoint: process.env.API_ENDPOINT || 'http://34.229.55.57:3000',
   baseUrl: process.env.BASE_URL || 'http://localhost:3000'
 }
