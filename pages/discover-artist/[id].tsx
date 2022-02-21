4de980bfa3251a56e9a3e93096289bcf01c77c3f
rahul gupta <rabbal1892@rabbal1892.local>
2022-02-20 19:31:00 -0600

    add card for listing page

--- pages/discover-artist/[id].tsx
+++ pages/discover-artist/[id].tsx
@@ -3,7 +3,7 @@ import Title from "components/title";
 import { useRouter } from "next/router";
 import landingPageImg from "public/images/listing.png";
 import dancersImg from "public/images/dancers.png";
-import { Card } from "antd";
+import { Card, Button } from "antd";
 import Link from "next/link";
 import { useRoutesContext } from "../../components/routeContext";
 import { routeToHref } from "config/routes";
@@ -53,43 +53,52 @@ const DiscoverArtist = ({
       if (artist !== null) {
         console.log("pp url : ", artist.profile_pic_url);
         resultArtists.push(
-          <div
-            key={index}
-            className="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3"
-            style={{ 
-              padding: "2rem"
-            }}
-          >
-            <Link
-              key={index}
-              href={routeToHref(toArtistProfile(artist.slug))}
-              passHref
-            >
-              <Card
-                hoverable
-                // className="discoverArtists__artistsContainer"
-                style={{ 
-                  height: "100%",
-                  //borderRadius: "15px",
-                  textAlign: "center",
-                  alignItems: "center",
-                  backgroundColor: "rgb(0 0 0 / 2%)",
-                }}
-                cover={
-                <Image 
-                  src={artist?.profile_pic_url} 
-                  alt="cards" 
-                  height={228}
-                  width={242}
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-12 col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                  <Image
+                    src={artist?.profile_pic_url}
+                    alt="cards"
+                    className="mx-auto img-fluid company-logo img-thumbnail rounded"
+                    height={150}
+                    width={150}
                   />
-                }
-              >
-                <Meta 
-                  title={artist.first_name} 
-                  description={artist.bio}
-                />
-              </Card>
-            </Link>
+                </div>
+                <div className="col-12 col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                  <h2 className="mb-0 font-weight-bold">{artist.first_name} {artist?.last_name}</h2>
+                  <h6 className="font-weight-normal">Dance, Singing, Poetry</h6>
+                  <p className="font-weight-normal">{artist.bio}</p>
+                </div>
+                <div className="col-12 col-lg-2 col-md-2 col-sm-12 detail">
+                  <Button>
+                    <Link
+                      key={index}
+                      href={routeToHref(toArtistProfile(artist.slug))}
+                      passHref
+                    > Profile </Link>
+                  </Button>
+                </div>
+                <div className="col-12 col-lg-2 col-md-2 text-center text-wrap detail">
+                    {/* <img *ngIf="item.payload.doc.data().price == 4" class="detail-icon" src="../../assets/images/extra/40.svg"> */}
+                    <p>Number of Collabs</p>
+                </div>
+
+                <div className="col-12 col-lg-2 col-md-2 col-sm-12 text-center text-wrap detail">
+                    {/* <img class="detail-icon" src="../../assets/images/extra/delivery.svg"> */}
+                    {/* <p *ngIf="item.payload.doc.data()['med delivery']">Delivery Available</p>
+                    <p *ngIf="!item.payload.doc.data()['med delivery']">Delivery Not Available</p> */}
+                     <p>Up for collab</p>
+                </div>
+
+                <div className="col-12 col-lg-2 col-md-2 col-sm-12 text-center text-wrap detail">
+                    {/* <img class="detail-icon" src="../../assets/images/extra/medical-insurance.svg"> */}
+                    {/* <p *ngIf="item.payload.doc.data().insurance">Insurance Accepted</p>
+                    <p *ngIf="!item.payload.doc.data().insurance">Insurance Not Accepted</p> */}
+                     <p>Price Range</p>
+                </div>
+              </div>
+            </div>
           </div>
         );
       }
@@ -101,17 +110,17 @@ const DiscoverArtist = ({
   return (
     <>
       <Title title="Discover Artist" />
-      <div className="fluid discoverArtists__listingContainer" style={{ marginTop: "10%", marginBottom: "15%" }}>
-          <div className="discoverArtists__coverContainer">
-            <Image 
-              layout="responsive" 
-              objectFit="contain"
-              src={landingPageImg} 
-              alt="Landing page" />
-          </div>
-          <div className="row">
-            {getArtists()}
-          </div>
+      <div className="fluid discoverArtists__listingPageContainer" style={{ marginTop: "10%", marginBottom: "15%" }}>
+        <div className="discoverArtists__coverContainer">
+          <Image
+            layout="responsive"
+            objectFit="contain"
+            src={landingPageImg}
+            alt="Landing page" />
+        </div>
+        <div className="col-12 listingContainer">
+          {getArtists()}
+        </div>
       </div>
     </>
   );
