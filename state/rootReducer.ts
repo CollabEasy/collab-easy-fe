63b24ca08ad3507011ddd71ce29c8173a9bf7767
rahul gupta <rabbal1892@rabbal1892.local>
2022-03-10 09:53:47 -0600

    some

--- state/rootReducer.ts
+++ state/rootReducer.ts
@@ -5,6 +5,7 @@ import user from "./reducers/userReducer";
 import collab from "./reducers/collabReducer";
 import sample from "./reducers/sampleReducer";
 import category from "./reducers/categoryReducer";
+import scratchpad from "./reducers/scratchpadReducer";
 
 const rootReducer = combineReducers<AppState>({
   home,
@@ -12,6 +13,7 @@ const rootReducer = combineReducers<AppState>({
   collab,
   sample,
   category,
+  scratchpad,
 });
 
 export default rootReducer
