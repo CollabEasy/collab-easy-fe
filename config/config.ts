4c26f24ef23b7a9ab62b02923a69276e576cfce2
Prashant Joshi <prashantjoshi@192.168.1.128>
2022-10-16 13:51:33 +0100

    Fixed date not updating for sent request

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,6 @@ import { AppEnvs, NodeEnvs } from "./environment";
 export const Config = {
   appEnv: process.env.APP_ENV || AppEnvs.DEVELOPMENT,
   nodeEnv: process.env.NODE_ENV || NodeEnvs.DEVELOPMENT,
-  backendEndpoint: process.env.API_ENDPOINT || 'http://wondor-lb-2124620248.us-east-1.elb.amazonaws.com/',
+  backendEndpoint: process.env.API_ENDPOINT || 'http://wondor-lb-2124620248.us-east-1.elb.amazonaws.com',
   baseUrl: process.env.BASE_URL || 'http://localhost:3000'
 }
