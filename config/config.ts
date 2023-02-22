df5eb50fcf969a86827efe77db327a308d8d0f8e
Rahul Gupta <rahulgupta6007@gmail.com>
2023-02-22 11:33:19 -0800

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,6 @@ import { AppEnvs, NodeEnvs } from "./environment";
 export const Config = {
   appEnv: process.env.APP_ENV || AppEnvs.DEVELOPMENT,
   nodeEnv: process.env.NODE_ENV || NodeEnvs.DEVELOPMENT,
-  backendEndpoint: process.env.API_ENDPOINT || 'https://api.wondor.art',
+  backendEndpoint: process.env.API_ENDPOINT || 'http://52.88.51.96:3001',
   baseUrl: process.env.BASE_URL || 'http://localhost:3000'
 }
