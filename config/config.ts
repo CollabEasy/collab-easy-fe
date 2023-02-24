f09ec6a39d234cf14030917d60d6bc9e39351fad
Rahul Gupta <rahulgupta6007@gmail.com>
2023-02-23 21:03:30 -0800

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,6 @@ import { AppEnvs, NodeEnvs } from "./environment";
 export const Config = {
   appEnv: process.env.APP_ENV || AppEnvs.DEVELOPMENT,
   nodeEnv: process.env.NODE_ENV || NodeEnvs.DEVELOPMENT,
-  backendEndpoint: process.env.API_ENDPOINT || 'http://52.88.51.96:3001',
+  backendEndpoint: process.env.API_ENDPOINT || 'http://35.91.155.191:3001',
   baseUrl: process.env.BASE_URL || 'http://localhost:3000'
 }
