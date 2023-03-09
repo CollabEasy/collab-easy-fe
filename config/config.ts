e415c5dd94c3e79970d6f13682b2495359b78e42
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-03-09 08:22:43 -0800

    update frontend url

--- config/config.ts
+++ config/config.ts
@@ -5,9 +5,9 @@ export const Config = {
   appEnv: process.env.APP_ENV || AppEnvs.DEVELOPMENT,
   nodeEnv: process.env.NODE_ENV || NodeEnvs.DEVELOPMENT,
 
+  baseUrl: process.env.BASE_URL || 'http://localhost:3000',
   // backendEndpoint: process.env.API_ENDPOINT || 'http://localhost:3001',
-  // baseUrl: process.env.BASE_URL || 'http://localhost:3000',
 
-  baseUrl: process.env.BASE_URL || 'https://main.d3s894l7qdv2do.amplifyapp.com',
+  // baseUrl: process.env.BASE_URL || 'https://main.d3s894l7qdv2do.amplifyapp.com',
   backendEndpoint: process.env.API_ENDPOINT || 'https://kejida35lh.execute-api.us-west-2.amazonaws.com/prod',
 }
