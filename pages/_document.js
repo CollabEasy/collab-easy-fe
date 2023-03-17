386a5dd0f7de94bd048fa171961015de188c797d
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-03-16 19:53:28 -0700

    commnt ga

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
+          {/* <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
               });
             `,
             }}
-          />
+          /> */}
 
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Nova+Round|Varela+Round&display=swap" rel="stylesheet"></link>
