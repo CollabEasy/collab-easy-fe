7361ff502147f5cc8bcdb1e0cc5fdf7263b510ea
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-03-23 14:55:11 -0700

    try one more time

--- components/profile.tsx
+++ components/profile.tsx
@@ -179,15 +179,16 @@ const Profile = ({
                     type="primary"
                     className="common-medium-btn"
                     style={{ height: 'auto', marginTop: '10px' }}
-                  // onClick={() => {
-                  //   setShowCollabModalState(true);
-                  //   setCollabRequestDetails(collab.collabDetails.sent.pending[0] ?? collab.collabDetails.sent.active[0]);
-                  // }}
+                  onClick={() => {
+                    setShowCollabModalState(true);
+                    setCollabRequestDetails(collab.collabDetails.sent.pending[0] ?? collab.collabDetails.sent.active[0]);
+                  }}
                   >
-                    <Link
+                    Show pending requests
+                    {/* <Link
                       href={routeToHref(toEditProfile("profile", "collab-request"))}
                       passHref
-                    >Show pending requests</Link>
+                    >Show pending requests</Link> */}
 
                   </Button>
                   <span className="common-text-style"><StarFilled style={{ color: 'orange', margin: '5px' }} />You have a pending collab request with {user.first_name}. </span>
