8d2a97a82a4d4c00a5ed629578de8b0da4573094
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-06-17 11:54:25 -0700

    added new themes

--- pages/get-inspired.tsx
+++ pages/get-inspired.tsx
@@ -83,45 +83,39 @@ const GetInspired = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, 
               <br></br>
             </p >
             <div className="theme">
-              <h5 className="card-text common-h5-style">Important events of the month</h5>
+              {/* <h5 className="card-text common-h5-style">Important events of the month</h5> */}
               <p className="card-text common-p-style">
-                1. World Day for Cultural Diversity for Dialogue and Development - May 21st
+                Celebrate the month of June with Wondor.art. Make art on any of topics below or beyond with the artists you have been wanting to work with on your next big hit!
               </p>
             </div>
             <div className="theme">
               <h5 className="card-text common-h5-style">Themes for the week</h5>
               <p className="card-text common-p-style">
-                1. <b>Emotions in Motion</b>: Use various artistic mediums to depict emotions in motion.
-                Capture the essence of joy, sadness, anger.
+                1. <b>Sun-inspired artwork:</b>: Create vibrant paintings or drawings that capture the warmth and radiance of the sun. Experiment with different color palettes to represent the changing hues of the sky during sunrise and sunset.
               </p>
               <p className="card-text common-p-style">
-                2. <b>Dreamscapes</b>: Let your imagination wander and create dreamlike landscapes or scenes.
-                Combine elements of reality and fantasy to depict surreal and whimsical environments
-                that evoke a sense of wonder and intrigue
+                2. <b>Nature&apos;s abundance</b>: Depict the lushness of nature during the summer season. Focus on blooming flowers, vibrant green landscapes, and abundant fruits and vegetables. Use various mediums like watercolors, pastels, or even create a nature-inspired collage.
               </p>
               <p className="card-text common-p-style">
-                3. <b>Human Connection</b>: Explore the theme of human connection and relationships.
-                Depict various forms of connections, such as friendships, family bonds, or
-                the universal ties that bring people together, emphasizing the significance of
-                interpersonal connections in our lives.
+                3. <b>Outdoor scenes</b>: Illustrate scenes of people enjoying outdoor activities that are popular during the summer months, such as picnics, beach trips, hiking, or playing sports. Capture the joy and energy of these moments in your artwork.
               </p>
               <p className="card-text common-p-style">
-                4. <b>Environmental Consciousness</b>: Address environmental issues through your artwork.
-                Shed light on topics like climate change, sustainability, or conservation. Encourage
-                viewers to reflect on their relationship with the natural world and the importance of
-                taking care of our planet.
+                4. <b>Light and shadow</b>: Explore the interplay of light and shadow during the longest day of the year. Experiment with different techniques to create dramatic effects and emphasize the changing angles of sunlight throughout the day.
+              </p>
+              <p className="card-text common-p-style">
+                5. <b>Symbolic representations</b>: Use symbols associated with the summer solstice, such as sunflowers, bonfires, fairies, or mythical creatures like dragons. These symbols can add a touch of magic and mystery to your artwork.
               </p>
             </div>
             <div className="theme">
               <h5 className="card-text common-h5-style">Quotes for the week</h5>
               <p className="card-text common-p-style">
-                1. Believe you can, and you are halfway there! - Abraham Lincoln
+                1. June is the gateway to summer! - Jean Hersey.
               </p>
               <p className="card-text common-p-style">
-                2. The only way to do great work is to love what you do! - Steve Jobs
+                2. June is the joyous month of harvest; the grass is green, the breeze is cool, and the sun&apos;s rays are golden! - Matshona Dhliwayo.
               </p>
               <p className="card-text common-p-style">
-                3. Every day may not be good, but there is something good in every day!
+                3. And what is so rare as a day in June? Then, if ever, come perfect days! - James Russell Lowell.
               </p>
             </div>
             <div className="theme">
