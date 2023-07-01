0191fcf782abfb9bb18cca74ccae96c6f4e0fdb2
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-07-01 09:13:56 -0700

    compare date instead of timestamp

--- helpers/contest.tsx
+++ helpers/contest.tsx
@@ -1,14 +1,30 @@
 
 export function GetDateString(time: number) {
     let myDate = new Date(time);
-    let dateStr = myDate.getFullYear() + "-" + (myDate.getMonth() + 1) + "-" + myDate.getDate();
+    let dateStr = myDate.getFullYear() + "-";
+    if (myDate.getMonth() + 1 < 10) {
+        dateStr += "0" + (myDate.getMonth() + 1) + "-";
+    } else {
+        dateStr += (myDate.getMonth() + 1) + "-";
+    }
+    
+    if (myDate.getDate() < 10) {
+        dateStr += "0" + myDate.getDate();
+    } else {
+        dateStr += myDate.getDate();
+    }
+
     return dateStr
 }
 
 export function GetContestStatus(now: number, start: number, end: number) {
-    if (now < start) {
+    var nowDateStr = GetDateString(now);
+    var startDateStr = GetDateString(start);
+    var endDateStr = GetDateString(end);
+
+    if (nowDateStr < startDateStr) {
         return "Upcoming";
-    } else if (now >= start && now <= end) {
+    } else if (nowDateStr >= startDateStr && nowDateStr <= endDateStr) {
         return "Ongoing";
     } else {
         return "Past";
