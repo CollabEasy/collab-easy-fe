9c7c4356c3027b31b58adb20214fffa6707535d7
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-07-05 20:10:26 -0700

    add more around inspo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,24 @@ import Title from '../components/title'
 import NewUserModal from '../components/modal/newUserModal';
 import LoginModal from '../components/loginModal';
 import Link from "next/link";
-import Script from 'next/script';
-import Head from 'next/head';
 import { Dispatch } from "redux";
 import { connect, ConnectedProps } from "react-redux";
 import Image from 'next/image';
-import landingDesktopImg from '../public/images/desktop-landing.svg';
-import landingMobileImg from '../public/images/mobile-landing.svg';
 import connectImg from '../public/images/connect.svg';
 import calendarImg from '../public/images/calendar.svg';
 import socialImg from '../public/images/social.svg';
-import photographerImg from '../public/images/popularCategories/photographer.svg'
-import paintersImg from '../public/images/popularCategories/painter.svg';
-import doodlingImage from '../public/images/popularCategories/doodling.svg';
+import cameramanImg from '../public/images/popularCategories/cameraman.svg'
 import writerImg from '../public/images/popularCategories/writer.svg';
-import sketchingImage from '../public/images/popularCategories/sketching.svg';
-import handLetteringImage from '../public/images/popularCategories/handLettering.svg';
 import contestTileImage from '../public/images/contestTile.svg';
 import { routeToHref } from "config/routes";
 
-import inspireImg from '../public/images/inspire.svg';
-import ideaImg from '../public/images/idea.svg';
-import { Card } from 'antd';
+import inspireImg from '../public/images/inspirebg.svg';
+import { Button, Card } from 'antd';
 import { useRoutesContext } from "components/routeContext";
-import { data } from 'copy';
 import { updateLoginData } from 'state/action';
 import React, { useEffect, useState } from 'react';
 import { LoginModalDetails } from 'types/model';
 import { AppState } from 'types/states';
-import { getLoginDetails } from 'helpers/helper';
-import Search from '@/components/search';
 
 const { Meta } = Card;
 
@@ -98,7 +86,7 @@ const Home = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
       <div className="row" style={{ padding: "20px", display: "flex" }}>
         <div className="container">
           <div className="column" style={{ padding: "10px" }}>
-            <div className="large-card" style={{background: "#E2F0CB"}}>
+            <div className="large-card" style={{ background: "#E2F0CB" }}>
               <Image
                 src={socialImg}
                 height={300}
@@ -107,14 +95,14 @@ const Home = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
               />
               <div>
                 <h3 className="common-h3-style">Discover New Opportunities</h3>
-                <p className="common-p-style">Join vibrant community of artists, networking and interact with peers. 
-                  Explore the profiles of fellow creators, discover new talent, exchange ideas, 
+                <p className="common-p-style">Join vibrant community of artists, networking and interact with peers.
+                  Explore the profiles of fellow creators, discover new talent, exchange ideas,
                   and initiate collaborations directly within the platform.</p>
               </div>
             </div>
           </div>
           <div className="column" style={{ padding: "10px" }}>
-            <div className="small-card" style={{ background: "#DBECFD"}}>
+            <div className="small-card" style={{ background: "#DBECFD" }}>
               <div className="small-card-text">
                 <h3 className="common-h3-style">Connect to Collaborate</h3>
                 <p className="common-p-style">Create a centralized profile,
@@ -128,9 +116,9 @@ const Home = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                   width={250}
                   priority
                 />
+              </div>
             </div>
-            </div>
-            <div className="small-card" style={{background: "#FBF0C4"}}>
+            <div className="small-card" style={{ background: "#FBF0C4" }}>
               <div className="small-card-text">
                 <h3 className="common-h3-style">Stremline Workflows</h3>
                 <p className="common-p-style">Easy-to-use workflow to send
@@ -150,147 +138,137 @@ const Home = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
         </div>
       </div>
 
-      <div className="row" style={{ height: "500px", backgroundColor: "#FAFCFE", padding: "20px", display: "flex" }}>
+      <div className="row" style={{ height: "500px", backgroundColor: "#E0EFEC", padding: "20px", display: "flex" }}>
         <div style={{ width: "30%", wordWrap: "break-word" }}>
           <h3 className="common-h3-style">Popular Categories for Collaboration</h3>
         </div>
         <div className="row mt-2 g-4">
-          <div className="col-md-3">
-            <div className="card p-1">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Mobile</span> <span>Phones</span> </div>
-                <div> <img src="https://i.imgur.com/b9zkoz0.jpg" height="100" width="100" /> </div>
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Creative Writings</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Head</span> <span>Phones</span> </div>
-                <div> <img src="https://i.imgur.com/SHWASPG.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Doodling</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Smart</span> <span>Watches</span> </div>
-                <div> <img src=" https://i.imgur.com/Ya0OXCv.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Hand Lettering</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Air</span> <span>Purifiers</span> </div>
-                <div> <img src="https://i.imgur.com/2gvGwbh.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Illustration</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2tr5">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Vacuum</span> <span>Cleaners</span> </div>
-                <div> <img src="https://i.imgur.com/UMQJpSG.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Photography</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Washing</span> <span>Machines</span> </div>
-                <div> <img src="https://i.imgur.com/e9CyhXR.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" style={{ backgroundColor: "#FFFFF" }}>
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Sketching</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Smart</span> <span>Televisions</span> </div>
-                <div> <img src=" https://i.imgur.com/Zq8VigZ.png" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Painting</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-          <div className="col-md-3">
-            <div className="card p-2">
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <div className="flex-column lh-1 imagename"> <span>Laptops</span> </div>
-                <div> <img src="https://i.imgur.com/6pK5oZl.jpg" height="100" width="100" /> </div>
+
+          <div className="col-md-3" style={{ cursor: 'pointer' }} >
+            <Link href={toArtist().href + 'creative-writing'} passHref>
+              <div className="card p-1" >
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="flex-column lh-1 imagename"> <span>Dancing</span> </div>
+                  <div> <Image src={cameramanImg} height={130} width={130} /> </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
 
-      <div className='card-rw'>
-        <h2 className="custom-padding common-h2-style">Popular Collab Categories</h2>
-        <div className="row text-center flex-row flex-nowrap mt-4 pb-4 pt-2 scrolling-wrapper">
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'creative-writing'} passHref>
-              <Card hoverable style={{ height: '100%' }} cover={<Image src={writerImg} alt="cards" />}>
-                <Meta className="common-text-style" title="Creative Writing" />
-              </Card>
-            </Link>
-          </div>
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'doodling'} passHref>
-              <Card hoverable style={{ height: '100%' }} cover={<Image src={doodlingImage} alt="cards" />}>
-                <Meta className="common-text-style" title="Doodling" />
-              </Card>
-            </Link>
-          </div>
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'hand-lettering'} passHref >
-              <Card hoverable style={{ height: '100%' }} cover={<Image src={handLetteringImage} alt="cards" />}>
-                <Meta className="common-text-style" title="Hand Lettering" />
-              </Card>
-            </Link>
-          </div>
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'photography'} passHref>
-              <Card hoverable style={{ width: '100%' }} cover={<Image src={photographerImg} alt="cards" />}>
-                <Meta className="common-text-style" title="Photography" />
-              </Card>
-            </Link>
-          </div>
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'sketching'} passHref>
-              <Card hoverable style={{ height: '100%' }} cover={<Image src={sketchingImage} alt="cards" />}>
-                <Meta className="common-text-style" title="Sketching" />
-              </Card>
-            </Link>
-          </div>
-          <div className="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-            <Link href={toArtist().href + 'painting'} passHref>
-              <Card hoverable style={{ height: '100%' }} cover={<Image src={paintersImg} alt="cards" />}>
-                <Meta className="common-text-style" title="Painting" />
-              </Card>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="row custom-padding">
-          <div style={{ cursor: 'pointer' }} className="col-12 col-sm-12 col-md-4 col-xl-4">
-            <Link href={routeToHref(toGetInspired())} passHref>
-              <Card style={{ height: '100%' }} cover={<Image src={inspireImg} alt="cards" />}>
-                <Meta className="common-text-style" title={<span style={{ whiteSpace: 'initial' }}>Searching for an idea for your next hit? We got you covered 🥳 </span>} />
-              </Card>
-            </Link>
-          </div>
-          <div style={{ cursor: 'pointer' }} className="col-12 col-sm-12 col-md-4 col-xl-4">
-            <Link href={routeToHref(toEditProfile("profile", "scratchpad"))} passHref>
-              <Card style={{ height: '100%' }} cover={<Image src={ideaImg} alt="cards" sizes="small" />}>
-                <Meta className="common-text-style" title={<span style={{ whiteSpace: 'initial' }}>Got an idea? Add it to your scratchpad before you forget 😎 </span>} />
-              </Card>
-            </Link>
-          </div>
-          <div style={{ cursor: 'pointer' }} className="col-12 col-sm-12 col-md-4 col-xl-4">
-            <Link href={routeToHref(toAllContestPage())} passHref>
-              <Card style={{ height: '100%' }} cover={<Image src={contestTileImage} alt="cards" />}>
-                <Meta className="common-text-style" title={<span style={{ whiteSpace: 'initial' }}>Want to win a prize? enter Wondor monthly contests 🤑 </span>} />
-              </Card>
-            </Link>
+      <div className="row" style={{ height: "350px" }} >
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-12">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="text-center p-4">
+                    <Image
+                      src={inspireImg}
+                      height={400}
+                      width={400}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="p-4">
+                    <div className="mt-4 mb-3 inspire-text">
+                      <h3 className="common-h3-style">Searching for an idea for your next hit? We got you covered 🥳</h3>
+                      <p className="common-p-style">
+                        We provide invaluable support to artists in times of need by publishing curated
+                        lists of themes and topics, serving as inspiration for their next creative masterpiece.
+                      </p>
+                    </div>
+                    <div className="mt-4 align-items-center">
+                      <div>
+                        <Button
+                          type="primary"
+                        >
+                          <Link href={routeToHref(toGetInspired())} passHref > Get Inspired </Link>
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
