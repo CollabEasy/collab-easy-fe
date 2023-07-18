b42b8602c4ef21e0e74e295c8df396507b57e540
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-07-18 07:03:53 -0700

    fix slug typo

--- helpers/categoryHelper.tsx
+++ helpers/categoryHelper.tsx
@@ -3,7 +3,7 @@ import { CATEGORY_WIKI } from "config/categoryWiki";
 export function GetCategoryWikiData(selectedCategorySlug) {
     let general = {};
     for (var i = 0; i < CATEGORY_WIKI.length; i++) {
-        if (CATEGORY_WIKI[i]["slugs"] === selectedCategorySlug) {
+        if (CATEGORY_WIKI[i]["slug"] === selectedCategorySlug) {
             return CATEGORY_WIKI[i];
         }
     }
