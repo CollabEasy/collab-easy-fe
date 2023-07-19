466846fb9ebe8b3175cc8c90a37b31e9978c30ce
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-07-19 16:27:46 -0700

    lol

--- constants/listing.ts
+++ constants/listing.ts
@@ -242,40 +242,127 @@ export const SIMILAR_CATEGORIES = [
 export const LISTING_METADATA = [
     {
         "slug": "art-journaling",
-        "meta_title": "Art journalers available for collaboration",
+        "meta_title": "Top art journalers available for collaboration",
         "meta_content": "Connect with like-minded art journalers, share your visual stories, and find endless inspiration.",
     },
     {
         "slug": "bullet-journaling",
-        "meta_title": "Bullet journalers available for collaboration",
+        "meta_title": "Top bullet journalers available for collaboration",
         "meta_content": "Connect with like-minded bulltet journalers, track your goals, and unlock your productivity potential.",
     },
     {
         "slug": "calligraphy",
-        "meta_title": "Calligraphers available for collaboration",
+        "meta_title": "Top calligraphers available for collaboration",
         "meta_content": "Connect with fellow calligraphers, hone your skills, and showcase your intricate lettering.",
     },
     {
         "slug": "caricature-drawing",
-        "meta_title": "Caricature artists available for collaboration",
+        "meta_title": "Top caricature artists available for collaboration",
         "meta_content": "Connect with fellow caricature artists, showcase your unique style, and unleash your humor and creativity as a caricature artist.",
     },
     {
         "slug": "choreography",
-        "meta_title": "Coreographers available for collaboration",
+        "meta_title": "Top coreographers available for collaboration",
         "meta_content": "Connect with fellow coreographers, craft mesmerizing routines, and inspire audiences.",
     },
     {
         "slug": "collage-making",
-        "meta_title": "Collage makers available for collaboration",
+        "meta_title": "Top collage makers available for collaboration",
         "meta_content": "Connect with like-minded collage makers, experiment with mixed media, and unleash your artistic expression through collage making.",
     },
-
-
-
+    {
+        "slug": "creative-journaling",
+        "meta_title": "Top creative journalers available for collaboration",
+        "meta_content": "Connect with like-minded creative journalers, share imaginative entries, and find inspiration for your expressive journey.",
+    },
+    {
+        "slug": "creative-writing",
+        "meta_title": "Top creative writers available for collaboration",
+        "meta_content": "Connect with like-minded creative writers, create, and explore captivating stories, poems, articles etc.",
+    },
+    {
+        "slug": "dancing",
+        "meta_title": "Top dancers available for collaboration",
+        "meta_content": "Connect with like-minded dancers, showcase your moves, and embrace the rhythm of tunes in the world of dance.",
+    },
+    {
+        "slug": "digital-art",
+        "meta_title": "Top digital artists available for collaboration",
+        "meta_content": "Connect with fellow digital artists, explore creative ideas, and craft compelling work.",
+    },
+    {
+        "slug": "doodling",
+        "meta_title": "Top doodlers available for collaboration",
+        "meta_content": "Connect with fellow doodlers, share your whimsical creations, and let your imagination shine by working with others.",
+    },
+    {
+        "slug": "guitar-playing",
+        "meta_title": "Top guitarist available for collaboration",
+        "meta_content": "Connect with fellow guitarists, share your melodies, harmonize with the community and strum your way to greatness.",
+    },
+    {
+        "slug": "hand-lettering",
+        "meta_title": "Top hand letterers available for collaboration",
+        "meta_content": "Master the art of hand lettering. Connect with skilled artists, showcase your elegant designs, and elevate your craft.",
+    },
+    {
+        "slug": "illustration",
+        "meta_title": "Top illustrators available for collaboration",
+        "meta_content": "Unleash your imagination as an illustrator. Connect with fellow artists, showcase your visual stories, and find endless inspiration.",
+    },
+    {
+        "slug": "journaling",
+        "meta_title": "Top journalers available for collaboration",
+        "meta_content": "Connect with like-minded illustrators, express, and document your journey.",
+    },
+    {
+        "slug": "musicians",
+        "meta_title": "Top musicians available for collaboration",
+        "meta_content": "Join fellow musicians in a harmonious space. Connect, collaborate, and share your melodies dedicated to passionate music makers.",
+    },
+    {
+        "slug": "painting",
+        "meta_title": "Top painters available for collaboration",
+        "meta_content": "Immerse yourself in the world of painting. Connect with fellow artists, exhibit your masterpieces, and embrace the colors.",
+    },
+    {
+        "slug": "photography",
+        "meta_title": "Top photographers available for collaboration",
+        "meta_content": "Capture moments, share stories. Connect with photographers, showcase your artistry, and explore the world through lenses.",
+    },
+    {
+        "slug": "poetry",
+        "meta_title": "Top poets available for collaboration",
+        "meta_content": "Compose verses that touch hearts. Connect with fellow poets, share your emotions, and let your words paint vivid emotions.",
+    },
+    {
+        "slug": "scrapbooking",
+        "meta_title": "Top scrapbookers available for collaboration",
+        "meta_content": "Preserve memories with scrapbooking. Connect with enthusiasts, showcase your creative layouts, and cherish life's moments.",
+    },
+    {
+        "slug": "singing",
+        "meta_title": "Top singers available for collaboration",
+        "meta_content": "Elevate your voice through singing. Connect with vocalists, share your melodies, and let the music resonate for passionate singers.",
+    },
+    {
+        "slug": "sketching",
+        "meta_title": "Top sketchers available for collaboration",
+        "meta_content": "Connect with sketchers, share your sketches, and explore endless possibilities on wondor for passionate sketchers.",
+    },
+    {
+        "slug": "spoken-words",
+        "meta_title": "Top spoken words artists available for collaboration",
+        "meta_content": "Embrace the power of spoken word. Connect with fellow artists, share your compelling performances, and ignite minds on wondor for spoken word artists.",
+    },
+    {
+        "slug": "standup-comedy",
+        "meta_title": "Top standup comedians available for collaboration",
+        "meta_content": "Spread laughter with stand-up comedy. Connect with comedians, share your humor, and bring joy to audiences on wondor for stand-up performers.",
+    },
     {
         "slug": "writing",
         "meta_title": "Writers available for collaboration",
-        "meta_content": "Connect with fellow writers, explore creative ideas, and craft compelling stories on our platform.",
+        "meta_content": "Connect with fellow writers, explore creative ideas, and craft compelling stories on wondor.",
     }
 ]
