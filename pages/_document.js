2699e739006cc3718999a521c95bee83ea239942
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-10-18 20:57:39 -0700

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
 
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Nova+Round|Varela+Round&display=swap" rel="stylesheet"></link>
+          <link rel="icon" href="favicon.ico" />
         </Head>
         <body>
           <Main />
