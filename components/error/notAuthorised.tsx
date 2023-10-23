d23c598c70003c5cca94ae6f1252167771ad0689
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-10-23 15:22:29 -0700

    Added login button to the not auth

--- components/error/notAuthorised.tsx
+++ components/error/notAuthorised.tsx
@@ -1,34 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { Dispatch } from "redux";
 import { connect, ConnectedProps } from "react-redux";
-import { Result } from "antd";
+import { Button, Result } from "antd";
 import Layout from "../layout";
+import { AppState } from "state";
+import LoginModal from "../modal/loginModal";
+import NewUserModal from "../modal/newUserModal";
+import { LoginModalDetails } from "types/model";
+import { openLoginModalAction } from "state/action";
+
+const mapStateToProps = (state: AppState) => {
+  const user = state.user.user;
+  const isLoggedIn = state.user.isLoggedIn;
+  const loginModalDetails = state.home.loginModalDetails;
+  return { user, isLoggedIn, loginModalDetails };
+};
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
+  openLoginModalAction: () => dispatch(openLoginModalAction()),
 });
 
-const connector = connect(null, mapDispatchToProps);
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type Props = {
-  error
+  error,
+  loginModalDetails: LoginModalDetails,
+  user: any,
 } & ConnectedProps<typeof connector>;
 
-export const NotAuthorised = (
+const NotAuthorised = ({
   error,
-) => {
+  user,
+  loginModalDetails,
+  openLoginModalAction,
+}: Props) => {
 
-  let message = error.error.length !== 0 ? error.error : "Create a new account or log in to your existing account to get the most from wondor!";
+  const [showProfileModal, setShowProfileModal] = useState(false);
 
+  const openLoginModal = () => {
+    openLoginModalAction();
+  };
+
+  let message = error.length !== 0 ? error : "Create a new account or log in to your existing account to get the most from wondor!";
   return (
     <Layout
       title={"Join Wondor now!"}
       name={"description"}
       content="Create a new account or log in to your existing account to get the most from wondor!"
     >
-      <div className="fluid discoverArtists__listingPageContainer" style={{ marginTop: "25%", marginBottom: "15%" }}>
+      {loginModalDetails.openModal && !user.new_user && <LoginModal />}
+      {showProfileModal && <NewUserModal />}
+
+      <div className="fluid discoverArtists__listingPageContainer" style={{ marginTop: "20%", marginBottom: "15%" }}>
         <div className="discoverArtists__listingPageCoverContainer common-text-style">
           <Result
             title={message}
+            extra={
+              <Button
+                className="common-text-style"
+                type="primary"
+                onClick={openLoginModal}
+              >
+                Log in
+              </Button>
+            }
           />
         </div>
       </div>
@@ -36,4 +71,4 @@ export const NotAuthorised = (
   );
 };
 
-export default NotAuthorised;
+export default connector(NotAuthorised);
