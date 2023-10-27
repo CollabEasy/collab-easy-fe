4960095067bb7032fb817a88f587f46592faa4a8
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-10-26 19:21:13 -0700

    add new file

---
+++ constants/inspirationIdeas.ts
@@ -0,0 +1,13 @@
+export const TIME_ZONES = [
+    {
+      "category": "Dancing",
+      "type": "Themes",
+      "value": [
+        "Festivity traditions and celebrations",
+        "The spirit of giving and generosity",
+        "The importance of family and friends",
+        "The magic of the holiday season",
+        "The joy of giving and receiving gifts",
+      ]
+    },
+  ]
