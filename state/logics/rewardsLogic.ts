a7aeedc43b27307f1ab037db0bcc6f6554491374
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-11-29 17:35:18 -0800

    fic rewards point for referral

--- state/logics/rewardsLogic.ts
+++ state/logics/rewardsLogic.ts
@@ -61,7 +61,7 @@ export const verifyRefferalCodeLogic = createLogic<
       const result = await rewardsApi.verifyRefferalCode(refferalCode);
       dispatch(actions.verifyRefferalCodeSuccess([result]));
       dispatch(actions.setIsReferralDone());
-      dispatch(notifActions.showNotification(true, 'Woo Hoo, you have earned 100 points by referral program  🥳'));
+      dispatch(notifActions.showNotification(true, 'Woo Hoo, you have earned 150 points by referral program  🥳'));
     } catch (error) {
       const error_response = error.response.data;
       dispatch(notifActions.showNotification(false, error_response['err_str']));
