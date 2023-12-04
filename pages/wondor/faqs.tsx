a2830db00a300f314f28e3a85e08b46ec114aa11
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-12-03 20:37:10 -0800

    add faqs

--- pages/wondor/faqs.tsx
+++ pages/wondor/faqs.tsx
@@ -39,7 +39,7 @@ type Props = {
 } & ConnectedProps<typeof connector>;
 
 
-const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistListData, faqContent }) => {
+const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistListData, generalFaqContent }) => {
     const [showProfileModal, setShowProfileModal] = useState(false);
     const { toContactUs, toDiscover } = useRoutesContext();
     const [windowWidth, setWindowWidth] = useState(-1);
@@ -59,14 +59,14 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
         }
     }, [artistListData]);
 
-    const getFAQCard = () => {
+    const getFAQfaqCard = () => {
         return (
             <div className="row">
                 <div style={{ width: "100%" }}>
                     <div className="row d-flex justify-content-center">
                         <div className="col-md-12" >
                             <Collapse ghost accordion>
-                                {faqContent.map((question, index) => (
+                                {generalFaqContent.map((question, index) => (
                                     <Panel header={question.question} key={index}>
                                         <p className="common-p-style">{question.answer}</p>
                                     </Panel>
@@ -102,7 +102,66 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                 }
                 <div className="faq-section-container">
                     <h1 className="common-h1-style">Frequently Asked Questions</h1>
-                    {getFAQCard()}
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-md-6">
+                                <div className="row">
+                                    <div className="col-12 grid-margin">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="mb-0">General Questions</h5>
+                                                </div>
+                                                <div>
+                                                    {getFAQfaqCard()}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12 grid-margin grid-margin-md-0">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="mb-0">Section 3</h5>
+                                                </div>
+                                                <div>
+                                                    {getFAQfaqCard()}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6">
+                                <div className="row">
+                                    <div className="col-12 grid-margin">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="mb-0">Section 2</h5>
+                                                </div>
+                                                <div>
+                                                    {getFAQfaqCard()}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="mb-0">Section 4</h5>
+                                                </div>
+                                                <div>
+                                                    {getFAQfaqCard()}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="row">
                     <GenericActionBanner />
@@ -112,7 +171,7 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
     )
 }
 
-const faqContent = [
+const generalFaqContent = [
     {
         question: "What is Wondor.art?",
         answer: "Wondor.art is a platform that connects artists with other artists who are available for collaboration, provides a space for artists to create collaboration proposals, participate in monthly contests, and find inspiration for their work. Wether you are painter, singer, musician, writer etc, Wondor serves for all of your artsy needs."
@@ -143,7 +202,7 @@ const faqContent = [
     },
     {
         question: "What is Wondor's loyalty program?",
-        answer: "The loyalty program is a program that rewards artists for helping to build the Wondor.art community. Artists can earn points by participating in contests, creating collaboration proposals, and referring other artists to the platform. Points can be redeemed for prizes, such as gift cards and discounts on Wondor.art services."
+        answer: "The loyalty program is a program that rewards artists for helping to build the Wondor.art community. Artists can earn points by participating in contests, creating collaboration proposals, and referring other artists to the platform. Points can be redeemed for prizes, such as gift faqCards and discounts on Wondor.art services."
     },
     {
         question: "How much does Wondor.art cost?",
@@ -163,10 +222,42 @@ const faqContent = [
     }
 ]
 
+
+const contestFaqContent = [
+    {
+        question: "What is Wondor.art?",
+        answer: "Wondor.art is a platform that connects artists with other artists who are available for collaboration, provides a space for artists to create collaboration proposals, participate in monthly contests, and find inspiration for their work. Wether you are painter, singer, musician, writer etc, Wondor serves for all of your artsy needs."
+    },
+    {
+        question: "Why should I use Wondor.art?",
+        answer: "Wondor is a one-stop destination for all of your artsy needs. There are many benefits to using Wondor, including seemless process for discovering artists up for collaboration to managing collaboration requests. If you are stuck in creativity block, you can use Inspiration Hub where latest trending themes, ideas are shared on weekly basis. You can pariticpate in our monthly art challenges to showcase your skill and win exclusive prizes. There are offering and you can learn about them as you start exploring Wondor's wonderful world."
+    },
+    {
+        question: "How can I find artists to collaborate with?",
+        answer: "You can simply use our search feature to find artists by their name or listed for a specific collab catgeory that you are interested in. Browse through the list and send collab requests using the easy-to-use collab tool."
+    },
+    {
+        question: "What types of projects are suitable for collaboration?",
+        answer: "Any artist with a set of skills can collaborate with other. Visual arts, music, literature, multimedia, design, cross-disciplinary, community art, performance, educational, and social impact projects are suitable for collaboration, fostering creativity across diverse creative fields."
+    },
+    {
+        question: "How can I create a collaboration proposal?",
+        answer: "To create a collaboration proposal, artists can simply fill out a form on the Wondor.art website. The form will ask artists to provide information about their project, such as the title, a brief description, the skills required etc. Interested artists can then show interest. Once you have enough interest, you can start collaboration with interested artists within seconds."
+    },
+    {
+        question: "How can I participate in monthly contests?",
+        answer: "To participate in monthly contests, artists can simply submit their work to the Wondor.art website. The deadline for submitting entries will be posted on the website."
+    },
+    {
+        question: "What is the Inspiration Hub?",
+        answer: "The Inspiration Hub is a collection of resources that is designed to help artists find inspiration for their work. Here, we post latest content creation ideas to help artists move past their creativity block and start their next creative project."
+    },
+]
+
 export async function getStaticProps({ params }) {
 
     // Pass post data to the page via props
-    return { props: { faqContent } }
+    return { props: { generalFaqContent } }
 }
 
 export default connector(FAQs);
