b9bdb59cc3d809016770d1f8b8e1ecd83df9a49d
Anchit <anchit@Yaminis-MacBook-Pro.local>
2023-12-05 19:32:39 +0530

    small change

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ const Home = ({
             {mainContent["paragraph"]}
           </p>
           <div className="hero-text-cnt-wrapper">
-            <button className="hero-text-container-button" style={{ backgroundColor: "#41A8F7", color: "white" }} onClick={openLoginModal}>
+            <button className="homepage-button" style={{ backgroundColor: "#41A8F7", color: "white" }} onClick={openLoginModal}>
               Join for Free!
             </button>
             <Link href={routeToHref(toTutorial())} passHref >
