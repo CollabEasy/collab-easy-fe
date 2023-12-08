b4fe3ff3958f05ae690670fea3b9515d9beaf95a
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-12-07 20:25:38 -0800

    add faqs

--- pages/wondor/faqs.tsx
+++ pages/wondor/faqs.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import { Button, Collapse } from "antd";
-import Link from "next/link";
-import contactUsImage from "public/images/contactUs.svg";
 import { useRoutesContext } from "components/routeContext";
 import { routeToHref } from "config/routes";
 
@@ -15,8 +11,16 @@ import React, { useEffect, useState } from 'react';
 import NewUserModal from '../../components/modal/newUserModal';
 import Layout from '@/components/layout';
 import GenericBreadcrumb from "@/components/genericBreadcrumb";
-import GenericActionBanner from "@/components/genericActionBanner";
 import GenericPageBanner from "@/components/genericPageBanner";
+import { Collapse } from "antd";
+import {
+    generalFaqContent, 
+    contestFaqContent, 
+    collaborationFaqContent, 
+    collaborationProposalFaqContent,
+    inspirationHubFaqContent,
+    securityAndAbuseFaqContent
+} from "../../constants/faqs";
 
 const { Panel } = Collapse;
 
@@ -40,7 +44,20 @@ type Props = {
 } & ConnectedProps<typeof connector>;
 
 
-const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistListData, generalFaqContent, contestFaqContent }) => {
+const FAQs = ({
+    isLoggedIn,
+    updateLoggedInData,
+    loginModalDetails,
+    user,
+    artistListData,
+    generalFaqContent,
+    contestFaqContent,
+    collaborationFaqContent,
+    collaborationProposalFaqContent,
+    inspirationHubFaqContent,
+    securityAndAbuseFaqContent,
+}) => {
+
     const [showProfileModal, setShowProfileModal] = useState(false);
     const { toContactUs, toDiscover } = useRoutesContext();
     const [windowWidth, setWindowWidth] = useState(-1);
@@ -115,7 +132,8 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                                         <div className="faqCard">
                                             <div className="faq-block faqCard-body">
                                                 <div className="container-fluid py-2">
-                                                    <h5 className="mb-0">General Questions</h5>
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">General Questions</h5>
+                                                    <div className="divider mb-2"> </div>
                                                 </div>
                                                 <div>
                                                     {getfaqCard(generalFaqContent)}
@@ -127,10 +145,24 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                                         <div className="faqCard">
                                             <div className="faq-block faqCard-body">
                                                 <div className="container-fluid py-2">
-                                                    <h5 className="mb-0">Art Contests</h5>
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">Art Contests</h5>
+                                                    <div className="divider mb-2"> </div>
                                                 </div>
                                                 <div>
                                                     {getfaqCard(contestFaqContent)}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12 grid-margin grid-margin-md-0">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">Inspiration Hub</h5>
+                                                    <div className="divider mb-2"> </div>
+                                                </div>
+                                                <div>
+                                                    {getfaqCard(inspirationHubFaqContent)}
                                                 </div>
                                             </div>
                                         </div>
@@ -143,10 +175,11 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                                         <div className="faqCard">
                                             <div className="faq-block faqCard-body">
                                                 <div className="container-fluid py-2">
-                                                    <h5 className="mb-0">Section 2</h5>
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">Collaboration Requests</h5>
+                                                    <div className="divider mb-2"> </div>
                                                 </div>
                                                 <div>
-                                                    {getfaqCard(generalFaqContent)}
+                                                    {getfaqCard(collaborationFaqContent)}
                                                 </div>
                                             </div>
                                         </div>
@@ -155,10 +188,24 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
                                         <div className="faqCard">
                                             <div className="faq-block faqCard-body">
                                                 <div className="container-fluid py-2">
-                                                    <h5 className="mb-0">Section 4</h5>
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">Collaboration Proposals</h5>
+                                                    <div className="divider mb-2"> </div>
                                                 </div>
                                                 <div>
-                                                    {getfaqCard(generalFaqContent)}
+                                                    {getfaqCard(collaborationProposalFaqContent)}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12 grid-margin grid-margin-md-0">
+                                        <div className="faqCard">
+                                            <div className="faq-block faqCard-body">
+                                                <div className="container-fluid py-2">
+                                                    <h5 className="common-h5-style f-19 mt-2 mb-2">Security and Abuse</h5>
+                                                    <div className="divider mb-2"> </div>
+                                                </div>
+                                                <div>
+                                                    {getfaqCard(securityAndAbuseFaqContent)}
                                                 </div>
                                             </div>
                                         </div>
@@ -173,102 +220,19 @@ const FAQs = ({ isLoggedIn, updateLoggedInData, loginModalDetails, user, artistL
     )
 }
 
-const generalFaqContent = [
-    {
-        question: "What is Wondor.art?",
-        answer: "Wondor.art is a platform that connects artists with other artists who are available for collaboration, provides a space for artists to create collaboration proposals, participate in monthly contests, and find inspiration for their work. Wether you are painter, singer, musician, writer etc, Wondor serves for all of your artsy needs."
-    },
-    {
-        question: "Why should I use Wondor.art?",
-        answer: "Wondor is a one-stop destination for all of your artsy needs. There are many benefits to using Wondor, including seemless process for discovering artists up for collaboration to managing collaboration requests. If you are stuck in creativity block, you can use Inspiration Hub where latest trending themes, ideas are shared on weekly basis. You can pariticpate in our monthly art challenges to showcase your skill and win exclusive prizes. There are offering and you can learn about them as you start exploring Wondor's wonderful world."
-    },
-    {
-        question: "How can I find artists to collaborate with?",
-        answer: "You can simply use our search feature to find artists by their name or listed for a specific collab catgeory that you are interested in. Browse through the list and send collab requests using the easy-to-use collab tool."
-    },
-    {
-        question: "What types of projects are suitable for collaboration?",
-        answer: "Any artist with a set of skills can collaborate with other. Visual arts, music, literature, multimedia, design, cross-disciplinary, community art, performance, educational, and social impact projects are suitable for collaboration, fostering creativity across diverse creative fields."
-    },
-    {
-        question: "How can I create a collaboration proposal?",
-        answer: "To create a collaboration proposal, artists can simply fill out a form on the Wondor.art website. The form will ask artists to provide information about their project, such as the title, a brief description, the skills required etc. Interested artists can then show interest. Once you have enough interest, you can start collaboration with interested artists within seconds."
-    },
-    {
-        question: "How can I participate in monthly contests?",
-        answer: "To participate in monthly contests, artists can simply submit their work to the Wondor.art website. The deadline for submitting entries will be posted on the website."
-    },
-    {
-        question: "What is the Inspiration Hub?",
-        answer: "The Inspiration Hub is a collection of resources that is designed to help artists find inspiration for their work. Here, we post latest content creation ideas to help artists move past their creativity block and start their next creative project."
-    },
-    {
-        question: "What is Wondor's loyalty program?",
-        answer: "The loyalty program is a program that rewards artists for helping to build the Wondor.art community. Artists can earn points by participating in contests, creating collaboration proposals, and referring other artists to the platform. Points can be redeemed for prizes, such as gift faqCards and discounts on Wondor.art services."
-    },
-    {
-        question: "How much does Wondor.art cost?",
-        answer: "Wondor.art is a free platform to use. There are no fees to create a profile, submit proposals, or participate in contests."
-    },
-    {
-        question: "Who can use Wondor.art?",
-        answer: "Wondor.art is open to all artists, content creators, regardless of their experience level."
-    },
-    {
-        question: "How can I get started using Wondor.art?",
-        answer: "To get started using Wondor.art, simply create a profile on the website. Once you have a profile, you can start connecting with other artists, creating collaboration proposals, and participating in contests."
-    },
-    {
-        question: "Does Wondor.art have a mobile app?",
-        answer: "No, Wondor.art does not have a mobile app. However, our website is mobile-responsive, which means you can easily access all collaboration tools on your smartphone or tablet."
-    }
-]
-
-
-const contestFaqContent = [
-    {
-        question: "Who can participate in the contest?",
-        answer: "Anyone with a Wondor account can participate in our contests."
-    },
-    {
-        question: "Are there any entry fees for the contest?",
-        answer: "No, there are no entry fees for our contests."
-    },
-    {
-        question: "What are the eligibility criteria for entering the contest?",
-        answer: "Eligibility criteria vary for each contest. Some are open to all artists, while others may require specific skills. You can find detailed criteria in the contest description."
-    },
-    {
-        question: "How are winners selected?",
-        answer: "Winners are chosen by the contest host, primarily based on the quality, creativity, and originality of submissions. Adherence to the theme is crucial, and submissions not aligned with the theme will be rejected."
-    },
-    {
-        question: "What are the prizes for the contest?",
-        answer: "Prizes are determined by the host and may vary. Winners receive a gift card, and all participants receive 50 Wondor rewards points."
-    },
-    {
-        question: "Is there a limit to the number of entries per participant?",
-        answer: "Yes, each participant can submit only one entry."
-    },
-    {
-        question: "Can I edit or withdraw my submission after it's been entered?",
-        answer: "No, you cannot edit or withdraw your submission once it's been entered. Submissions are opened for voting by other participants upon submission."
-    },
-    {
-        question: "How does the voting system work in contests?",
-        answer: "Contests are open to all, and Wondor users and participants can vote for submissions to show their support for fellow participants."
-    },
-    {
-        question: "Are winners decided solely based on votes?",
-        answer: "No, votes are tokens of appreciation and not the sole deciding factor in choosing the winner."
-    },
-];
 
 
 export async function getStaticProps({ params }) {
 
     // Pass post data to the page via props
-    return { props: { generalFaqContent, contestFaqContent } }
+    return { props: { 
+        generalFaqContent, 
+        contestFaqContent, 
+        collaborationFaqContent, 
+        collaborationProposalFaqContent,
+        inspirationHubFaqContent,
+        securityAndAbuseFaqContent
+     } }
 }
 
 export default connector(FAQs);
