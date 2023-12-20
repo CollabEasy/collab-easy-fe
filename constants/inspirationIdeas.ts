42fa029ce9b6ffe1df1f665848c135fab1449673
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-12-20 14:49:04 -0800

    add new themes

--- constants/inspirationIdeas.ts
+++ constants/inspirationIdeas.ts
@@ -1,5 +1,85 @@
 export const CURRENT_THEMES = [
     {
+        "title": "Parallel Universes Story",
+        "description": "Write a story where the characters exist simultaneously in different parallel universes, exploring how their lives differ.",
+        "categories": ["Writing"],
+    },
+    {
+        "title": "Time-Travel Journal Entries",
+        "description": "Craft a series of journal entries from a time traveler's perspective, detailing their experiences and observations in different historical periods.",
+        "categories": ["Writing"],
+    },
+    {
+        "title": "Future Predictions",
+        "description": "Write a speculative piece predicting what the world might be like in 50 years, considering technological advancements, societal changes, and environmental shifts.",
+        "categories": ["Writing"],
+    },
+    {
+        "title": "Microfiction Series",
+        "description": "Create a series of microfiction stories (very short stories) that each capture a moment in time, leaving readers with a powerful impression.",
+        "categories": ["Writing"],
+    },
+    {
+        "title": "Nature Haikus",
+        "description": "Write a collection of haikus that vividly describe different aspects of nature, capturing moments of beauty and serenity.",
+        "categories": ["Poetry"],
+    },
+    {
+        "title": "Emotional Landscapes",
+        "description": "Craft poems that explore the emotional landscapes of joy, sorrow, love, and introspection, using vivid metaphors and imagery.",
+        "categories": ["Poetry"],
+    },
+    {
+        "title": "Color Poems",
+        "description": "Create poems inspired by different colors, exploring the emotions and sensations associated with each hue.",
+        "categories": ["Poetry"],
+    },
+    {
+        "title": "Odes to Ordinary Things",
+        "description": "Write odes to everyday objects or experiences, finding the extraordinary in the mundane.",
+        "categories": ["Poetry"],
+    },
+    {
+        "title": "Genre Fusion Song",
+        "description": "Experiment with fusing two different music genres to create a unique and unexpected composition.",
+        "categories": ["Music"],
+    },
+    {
+        "title": "Storytelling Through Instrumentals",
+        "description": "Compose an instrumental piece that tells a story without the need for lyrics, using melodies and harmonies to convey emotions and narrative arcs.",
+        "categories": ["Music"],
+    },
+    {
+        "title": "Cultural Fusion Track",
+        "description": "Blend elements from different cultural musical traditions to create a multicultural and richly layered musical piece.",
+        "categories": ["Music"],
+    },
+    {
+        "title": "Nature-inspired Soundscapes",
+        "description": "Create ambient soundscapes inspired by different natural environments, incorporating elements like bird sounds, flowing water, and rustling leaves.",
+        "categories": ["Music"],
+    },
+    {
+        "title": "Contrast Series Photography",
+        "description": "Capture images that showcase stark contrasts, whether in colors, textures, or themes, to create visually striking compositions.",
+        "categories": ["Photography"],
+    },
+    {
+        "title": "Reflections in Water Photography",
+        "description": "Explore the beauty of reflections in water, capturing images that play with symmetry, distortion, and the interplay of light.",
+        "categories": ["Photography"],
+    },
+    {
+        "title": "Candid Portraits of Strangers Photography",
+        "description": "Challenge yourself to take candid portraits of strangers in public spaces, capturing authentic and unposed moments.",
+        "categories": ["Photography"],
+    },
+    {
+        "title": "Abstract Macro Photography",
+        "description": "Use macro photography to capture close-up shots of everyday objects, turning them into abstract and visually intriguing compositions.",
+        "categories": ["Photography"],
+    },
+    {
         "title": "Photo-Poetry Project",
         "description": "A photographer and a poet collaborate to create a series of works where each photograph inspires a poem, and vice versa.",
         "categories": ["Photography", "Poetry"],
@@ -7,12 +87,12 @@ export const CURRENT_THEMES = [
     {
         "title": "Graphic Novel Illustration",
         "description": "A writer and an illustrator team up to create a graphic novel, combining their talents to tell a compelling story with both words and images.",
-        "categories": ["Writing", "Illustration", "Digital Art", "Poetry"], 
+        "categories": ["Writing", "Illustration", "Digital Art", "Poetry"],
     },
     {
         "title": "Literature and Street Art",
         "description": "Collaborate with writers and poets to transform public spaces with street art installations that visually depict or respond to literary works, creating an outdoor gallery experience.",
-        "categories": ["Writing", "Poetry", "Painting"], 
+        "categories": ["Writing", "Poetry", "Painting"],
     },
     {
         "title": "Musical Storytelling Photography",
