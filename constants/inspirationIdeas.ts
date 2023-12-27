11b52fa846bbca4d7d7e53c3697c16f12c808014
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2023-12-27 10:47:19 -0800

    add topics for collage making

--- constants/inspirationIdeas.ts
+++ constants/inspirationIdeas.ts
@@ -1,5 +1,75 @@
 export const CURRENT_THEMES = [
     {
+        "title": "Dream Journal Collage",
+        "description": "Capture the essence of a vivid dream through collage. Incorporate snippets of text, magazine cutouts, found objects, and paint to create a surreal and layered visual representation of your dreamscape.",
+        "categories": ["art-journaling", "creative-journaling", "journaling"]
+    },
+    {
+        "title": "Monthly Calendar Spread",
+        "description": "Visually map out your month with a calendar spread that incorporates grids, trackers, and color-coding to highlight appointments, deadlines, and important dates.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Weekly Habit Tracker",
+        "description": "Monitor your progress on goals like exercising, reading, or meditating by creating a weekly habit tracker with checkboxes or creative icons.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Brain Dump Spread",
+        "description": "Declutter your mind and capture fleeting thoughts, tasks, or ideas by creating a dedicated brain dump spread in your bullet journal.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Book & Movie Log",
+        "description": "Design a fun spread for keeping track of books you've read and movies you've watched. Add personal ratings, memorable quotes, and small sketches to capture your experiences.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Mood Tracker",
+        "description": "Log your emotions each day using different colors or symbols to identify patterns and understand your emotional well-being.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Daily Affirmations",
+        "description": "Create a page dedicated to daily affirmations and positive quotes. Illustrate these empowering words with complementary visuals to boost your motivation and self-esteem.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Gratitude Log",
+        "description": "Boost your happiness by reflecting on and appreciating the good things in your life through a daily or weekly gratitude log.",
+        "categories": ["bullet-journaling"]
+    },
+    {
+        "title": "Emotional Landscape",
+        "description": "Create a visual representation of your current emotions using colors, textures, and shapes. Let your feelings guide your brushstrokes and material choices.",
+        "categories": ["art-journaling", "creative-journaling", "journaling"]
+    },
+    {
+        "title": "Poetry in Pictures",
+        "description": "Merge poetry and visuals by illustrating your favorite poems or creating visuals inspired by your own verses. Use mixed media to bring the words to life on the pages.",
+        "categories": ["art-journaling", "creative-journaling", "poetry"]
+    },
+    {
+        "title": "Gratitude Mandala",
+        "description": "Express your gratitude through a colorful mandala. Start by writing down things you're grateful for, then create a circular design using patterns, symbols, and words that represent your thankfulness.",
+        "categories": ["art-journaling", "creative-journaling", "journaling"]
+    },
+    {
+        "title": "Dreamscape Doodles",
+        "description": "Illustrate your dreams and aspirations through whimsical doodles. Create a visual representation of the surreal landscapes and ideas that inhabit your imagination.",
+        "categories": ["doodling"]
+    },
+    {
+        "title": "Mindful Mandalas",
+        "description": "Practice mindfulness through mandala creation. Let the meditative process guide your hand as you design intricate patterns and reflect on the balance and harmony within.",
+        "categories": ["art-journaling", "creative-journaling", "sketching"]
+    },
+    {
+        "title": "Song Lyric Art",
+        "description": "Choose a song that resonates with you and create an art journal spread inspired by its lyrics. Use visual elements to represent the emotions, imagery, and themes of the song.",
+        "categories": ["art-journaling", "creative-journaling", "journaling"]
+    },
+    {
         "title": "Parallel Universes Story",
         "description": "Write a story where the characters exist simultaneously in different parallel universes, exploring how their lives differ.",
         "categories": ["Writing"],
@@ -8,6 +78,26 @@ export const CURRENT_THEMES = [
         "title": "Time-Travel Journal Entries",
         "description": "Craft a series of journal entries from a time traveler's perspective, detailing their experiences and observations in different historical periods.",
         "categories": ["Writing"],
+    },
+    {
+        "title": "Whimsical Childhood Adventures",
+        "description": "Unleash your inner child and recreate the magic of your early years in a playful scrapbook. Use nostalgic embellishments, colorful photos, and handwritten stories to capture those joyous moments that make you smile. Share anecdotes, showcase your favorite toys, and preserve those cherished memories that shaped your journey.",
+        "categories": ["scrapbooking", "collage-making"]
+    },  
+    {
+        "title": "Travel Chronicles: Wanderlust Unbound",
+        "description": "Embark on a captivating journey through your travel memories with a scrapbook that ignites wanderlust. Map out your adventures with vintage tickets, cherished souvenirs, and vibrant photos that transport you back to those awe-inspiring moments. Weave tales of exploration, cultural encounters, and personal growth, creating a treasured keepsake that celebrates the world's wonders.",
+        "categories": ["scrapbooking", "collage-making"]
+    },
+    {
+        "title": "Milestone Moments: A Celebration in Collage",
+        "description": "Mark life's most significant occasions with a visually captivating scrapbook collage that honors your achievements and cherished memories. Gather photos, mementos, and heartfelt words to create an artful tapestry of love, laughter, and growth. Showcase graduations, weddings, birthdays, career triumphs, and other milestones that have shaped your unique life story.",
+        "categories": ["scrapbooking", "collage-making"]
+    },
+    {
+        "title": "Pet Pal Album: Tails of Unconditional Love",
+        "description": "Honor the furry friends who hold a special place in your heart with a heartwarming scrapbook that celebrates their unique personalities and the joy they bring to your life. Fill the pages with candid snapshots, paw-printed mementos, and heartwarming stories that capture their playful antics, unconditional love, and the unbreakable bond you share. Preserve those precious moments of snuggles, walks in the park, and unconditional companionship in a cherished keepsake that will forever warm your heart.",
+        "categories": ["scrapbooking", "collage-making"]
     },
     {
         "title": "Future Predictions",
