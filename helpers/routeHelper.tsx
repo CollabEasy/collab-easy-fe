f1e1cd00ed93cd66c522bb7e9edb6f3ade8cc9b6
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2024-01-04 17:42:18 -0800

    fix the links on search page

--- helpers/routeHelper.tsx
+++ helpers/routeHelper.tsx
@@ -4,49 +4,49 @@ import { GetCategoryArtistTitle } from "./categoryHelper";
 export function GetCollabLink(slug) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/collab/artist/" + slug
-        : "");
+        : "/collab/artist/" + slug);
 }
 
 export function GetRefferalLink(code) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/login?code=" + code
-        : "");
+        : "/login?code=" + code);
 }
 
 export function GetBlogUrl(url) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + url
-        : "");
+        : url);
 }
 
 export function GetProposalUrl(url) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + url
-        : "");
+        : url);
 }
 
 
 export function GetArtistCollabPage(slug) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/collab/artist/" + slug
-        : "");
+        : "/collab/artist/" + slug);
 }
 
 
 export function GetAllProposals(slug) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/collab-proposals-for-artists?category=" + slug
-        : "");
+        : "/collab-proposals-for-artists?category=" + slug);
 }
 
 export function GetAllCategories() {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/collab-categories"
-        : "");
+        : "/collab-categories");
 }
 
 export function GetCategoryPage(slug) {
     return (typeof window !== "undefined" && window.location.origin
         ? window.location.origin + "/category/" + slug + "/" + GetCategoryArtistTitle(slug) + "-seeking-collab"
-        : "");
+        : "/category/" + slug + "/" + GetCategoryArtistTitle(slug) + "-seeking-collab");
 }
