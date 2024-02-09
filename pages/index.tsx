a7ffe00316e4c4766c2ac942305a6d71d685c335
Pooja Gupta <poojagupta@Poojas-MacBook-Air.local>
2024-02-09 15:40:55 +0530

    fix some more colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -724,14 +724,14 @@ const Home = ({
       <div className="row">
         <div className="curved-div upper">
           <svg viewBox="0 0 1440 319">
-            <path fill="#F4A547" fillOpacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            <path fill="#8A89F7" fillOpacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
         </div>
-        <div className="curved-div" style={{ background: "#F4A547" }}>
-          <h1>
+        <div className="curved-div" style={{ background: "#8A89F7" }}>
+          <h1 style={{color: "#071C75"}}>
             Wondor, A Community You Can Call Yours
           </h1>
-          <p className="common-p-style">
+          <p className="common-p-style" style={{color: "#071C75"}}>
             Designed for artists by an artist, Wondor is more than a platform; it&apos;s a global community connecting creative minds.
           </p>
           <svg viewBox="0 0 1440 319">
